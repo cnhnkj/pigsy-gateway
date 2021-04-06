81c7fd362b512c3565bed8fdd8e41e62dd9c5e55
dragonlong1986 <dragonlong1986@126.com>
2021-04-06 13:30:34 +0800

    提交一些基础环境的配置

--- README.md
+++ README.md
@@ -6,8 +6,15 @@
 
 # 依赖
 
+- `Spring-Boot`的`2.3.9.RELEASE`版本
+- `Spring-Cloud`的`Hoxton.SR10`版本
+- `Apollo`的`1.8.1`版本
 
 # 模块
+
+- `pigsy-gateway-eureka` 提供了核心的服务注册和发现功能，网关通过先从`eureka`模块获取数据，然后进行路由选择到具体的服务
+- `pigsy-gateway-apollo` 提供了配置中心功能，网关通过先从`apollo`模块实时获取配置数据，并且进行动态更新
+- `pigsy-gateway-auth` 提供了各种环境的权限认证的方式，可能认证要处理不同的业务逻辑，故独立成独立服务进行验证，把认证结果告知网关
 
 
 # 功能结构图
---
+++ pigsy-gateway-eureka/src/main/java/com/cnhnnkj/pigsy/eureka/EurekaServerApplication.java
@@ -0,0 +1,17 @@
+package com.cnhnnkj.pigsy.eureka;
+
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+
+  public static void main(String[] args) {
+    new SpringApplicationBuilder(EurekaServerApplication.class).web(WebApplicationType.SERVLET)
+        .run(args);
+  }
+}
